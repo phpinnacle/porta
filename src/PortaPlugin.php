0540387ba62559d11591d30a4b4ab0226e9230d3
Anton Shabouta <zloyusr@gmail.com>
2026-09-05 15:16:16 +0300

    [atlas+20]: Simplify inline returns

--- src/PortaPlugin.php
+++ src/PortaPlugin.php
@@ -14,6 +14,7 @@ class PortaPlugin implements Plugin
 
     public static function get(): static
     {
+        // @mago-expect lint:inline-variable-return
         /** @var static $plugin */
         $plugin = filament(app(static::class)->getId());
 
